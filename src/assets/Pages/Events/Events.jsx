e77f8d9566e9a68680308d4725466eca20612d22
Alin <alinsridhar1@gmail.com>
2026-02-16 20:55:20 +0530

    events dont have footer - resolved

--- src/assets/Pages/Events/Events.jsx
+++ src/assets/Pages/Events/Events.jsx
@@ -1,9 +1,11 @@
 import Navbar from "../../Components/Navbar/Navbar";
 import Ourevents from "../../Components/Ourevents/Ourevents";
+import Footer from "../../Components/Footer/Footer";
 export default function Events() {
 			  return (
               <>
               <Navbar/>
                 <Ourevents/>
+                <Footer/>
               </>
 );}
